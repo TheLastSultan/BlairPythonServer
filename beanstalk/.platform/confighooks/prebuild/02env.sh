4b43a3a028efbf08fc1d150f6632fe8602a28cbd
Ibrahim Ibrahim <104999073+Isquare007@users.noreply.github.com>
2025-04-09 15:20:36 +0100

    add open ai key

--- beanstalk/.platform/confighooks/prebuild/02env.sh
+++ beanstalk/.platform/confighooks/prebuild/02env.sh
@@ -15,4 +15,5 @@ touch .env
   printf "JWT_SIGNING_KEY=%s\n" "$(secretsmanager prod/rocketdevs ROCKETDEVS_JWT_SIGNING_KEY)"
   printf "HASURA_ENDPOINT=%s\n" "https://talent1.app/hasura/v1/graphql"
   printf "LARK_ERROR_WEBHOOK_URL=%s\n" "https://open.larksuite.com/open-apis/bot/v2/hook/3015b239-4fc4-4033-8e5a-63efe60c42ec"
+  printf "OPENAI_API_KEY=%s\n" "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"
 } > .env
